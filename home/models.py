f10b965640f8bac061867542359116129e7cfbe9
Andreas Siedler <andreas.siedler@gmail.com>
2020-07-19 00:48:06 +0200

    added linter

--- home/models.py
+++ home/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+    
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
