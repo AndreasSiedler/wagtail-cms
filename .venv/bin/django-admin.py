2015a64ad8ffc7feb3ec903c20986785678353a1
Andreas Siedler <andreas.siedler@gmail.com>
2020-07-19 00:43:06 +0200

    first commit

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/christinaneumayer/Documents/andreas/development/repos/wagtailraw/.venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
