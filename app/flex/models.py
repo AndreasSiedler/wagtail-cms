69202a1caa3d3e9ddd853a2bb05b7c8185b25231
Andreas Siedler <andreas.siedler@gmail.com>
2020-07-20 00:48:11 +0200

    prod ready

---
+++ app/flex/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django import forms
+
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import StreamFieldPanel, FieldPanel
+
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.core import blocks
+from wagtail.embeds.blocks import EmbedBlock
+
+# Create your models here.
+class FlexPage(Page):
+    intro = models.CharField(max_length=250, blank=True)
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('row', blocks.StructBlock([
+            ('layout', blocks.MultipleChoiceBlock(choices=[
+                ("test", "test2"),
+                ("test", "test2"),
+                ("test", "test2"),
+                ("test", "test2"),
+                ("test", "test2"),
+                ("test", "test2"),
+                ("test", "test2"),
+                ("test", "test2"),
+                ("test", "test2"),
+                ("test", "test2"),
+                ("test", "test2"),
+                ("test", "test2"),
+            ], widget=forms.SelectMultiple)),
+            ('carousel', blocks.StreamBlock(
+                [
+                    # ('columns', blocks.ListBlock([
+                    #     ('column', blocks.CharBlock()),
+                    # ])),
+                    ('image', ImageChooserBlock()),
+                    ('quotation', blocks.StructBlock([
+                        ('text', blocks.TextBlock()),
+                        ('author', blocks.CharBlock()),
+                    ])),
+                    ('video', EmbedBlock()),
+                ],
+                icon='cogs'
+            ))
+        ], icon='user')),
+
+    ])
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+        StreamFieldPanel('body'),
+    ]
