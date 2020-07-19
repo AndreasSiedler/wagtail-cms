f1e70913f23ce9bc66b30384a0ef80f68a88c6e0
Andreas Siedler <andreas.siedler@gmail.com>
2020-07-20 00:47:11 +0200

    Fixed gitignore cache

--- streams/blocks.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from wagtail.core import blocks
-from django.db import models
-
-from wagtail.core.fields import StreamField
-from wagtail.snippets.models import register_snippet
-from wagtail.snippets.blocks import SnippetChooserBlock
-
-
