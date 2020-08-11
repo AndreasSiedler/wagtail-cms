036750c48a8fe43e66f836a07ecd3b84e4017019
Andreas Siedler <andreas.siedler@gmail.com>
2020-08-11 23:23:23 +0200

    Added new blocks

--- app/streams/blocks.py
+++ app/streams/blocks.py
@@ -1,24 +1,47 @@
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.embeds.blocks import EmbedBlock
+from wagtail.images.blocks import ImageChooserBlock
 
-class Design(blocks.StreamBlock):
-    padding = blocks.ChoiceBlock(choices=[
-        ('Padding 2', 'p-2')
-    ])
-    margin = blocks.ChoiceBlock(choices=[
-        ('Margin 2', 'm-2')
-    ])
+COLOUR_CHOICES = [
+     ("red", "red-value"),
+     ("blue", "blue-value")
+ ]
+
+class GoogleMapBlock(blocks.StructBlock):
+    map_long = blocks.CharBlock(required=True,max_length=255)
+    map_lat = blocks.CharBlock(required=True,max_length=255)
+    map_zoom_level = blocks.CharBlock(default=14,required=True,max_length=3)
+ 
+    class Meta:
+        template = 'yourapp/blocks/google_map.html'
+        icon = 'cogs'
+        label = 'Google Map'
 
 class TextAndImage(blocks.StructBlock):
-    design = Design()
-    title = blocks.CharBlock(required=False, help_text="Add your Title")
-    text = blocks.RichTextBlock(required=True, help_text="Add your Text")
-    image = ImageChooserBlock(required=True, help_text="Choose your Image")
-
+    background = blocks.ChoiceBlock(choices=COLOUR_CHOICES,default="white")
+    text = blocks.CharBlock()
+    left_column = blocks.StreamBlock([
+            ('heading', blocks.CharBlock(classname="full title")),
+            ('paragraph', blocks.RichTextBlock()),
+            ('image', ImageChooserBlock()),
+            ('embedded_video', EmbedBlock()),
+            ('google_map', GoogleMapBlock()),
+        ], icon='arrow-left', label='Left column content')
+ 
+    right_column = blocks.StreamBlock([
+            ('heading', blocks.CharBlock(classname="full title")),
+            ('paragraph', blocks.RichTextBlock()),
+            ('image', ImageChooserBlock()),
+            ('embedded_video', EmbedBlock()),
+            ('google_map', GoogleMapBlock()),
+        ], icon='arrow-right', label='Right column content')
+ 
     class Meta:
-        template = "streams/text_image_block.html"
-        icon = "view"
-        label = "Text and Image"
+        template = 'yourapp/blocks/two_column_block.html'
+        icon = 'placeholder'
+        label = 'Two Columns'
+
 
 class TitleAndText(blocks.StructBlock):
     title = blocks.CharBlock(required=True, help_text="Add your Title")
