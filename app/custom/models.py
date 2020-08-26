6c0cc15292bcbdeee100f44e9ee90efc22b3db6f
Andreas Siedler <andreas.siedler@gmail.com>
2020-08-26 20:52:08 +0000

    Updated custom settings app

--- app/custom/models.py
+++ app/custom/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.contrib.settings.models import BaseSetting, register_setting
+from wagtail.admin.edit_handlers import (
+    ObjectList,
+    TabbedInterface,
+)
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail_color_panel.fields import ColorField
+from wagtail_color_panel.edit_handlers import NativeColorPanel
 
 
 # Create your models here.
@@ -12,12 +19,38 @@ class SocialMediaSettings(BaseSetting):
     twitter = models.URLField(blank=True, null=True, help_text="Twitter Url")
     youtube = models.URLField(blank=True, null=True, help_text="Youtube Url")
 
-    panels = [
+    header_logo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    header_background_color = ColorField(blank=True, null=True, help_text="Choose background color")
+
+    header_panels = [
+        MultiFieldPanel(
+            [
+                ImageChooserPanel('header_logo'),
+                NativeColorPanel('header_background_color'),
+            ]
+        )
+    ]
+
+    socia_media_panels = [
         MultiFieldPanel([
             FieldPanel("facebook"),
             FieldPanel("twitter"),
             FieldPanel("youtube"),
         ])
     ]
+
+    edit_handler = TabbedInterface(
+        [
+            ObjectList(socia_media_panels, heading="Social"),
+            ObjectList(header_panels, heading="Header"),
+        ]
+    )
+
 
 # register_setting(SocialMediaSettings)
